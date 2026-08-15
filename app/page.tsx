e4e1cd2f7062c1aae7c2375acb156ac313c3b478
vrodeprogramist <54kos_sh@mail.ru>
2026-08-15 21:56:09 +0700

    page.tsx
    Removed Header component and navigation array from Home component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,10 @@
-"use client";
-
-import Header from "@/components/ui/Header";
 import Hero from "@/components/ui/Hero";
-
-const navigation = [
-  { label: "Главная", href: "/" },
-  { label: "О бюро", href: "/oburo" },
-  { label: "Услуги", href: "/uslugi" },
-  { label: "Портфолио", href: "/portfolio" },
-  { label: "Контакты", href: "/contacts" },
-];
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-[#1c1212]">
-      <Header navigation={navigation} />
-      <Hero 
-        primaryButton={{
-          text: "Client Access",
-          onClick: () => console.log("Клик"),
-        }}
-        title=""
-        description="Эксклюзивная архитектура. Ваше приватное пространство. Мы работаем с ограниченным числом клиентов год, чтобы гарантировать максимальное внимание к деталям и полную конфиденциальность. Ваш проект будет уникальным, как отпечаток пальца, а процесс его создания - таким же закрытым."
-      />
-    </div>
+    <Hero 
+      title=""
+      description="Эксклюзивная архитектура. Ваше приватное пространство. Мы работаем с ограниченным числом клиентов год, чтобы гарантировать максимальное внимание к деталям и полную конфиденциальность. Ваш проект будет уникальным, как отпечаток пальца, а процесс его создания - таким же закрытым."
+    />
   );
 }
